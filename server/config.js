3aea7b86593551cf02dc394e58087f5437b3a765
Akshar Barchha <60133596+aksharbarchha@users.noreply.github.com>
2020-11-20 20:26:33 +0530

    Initial Commit

---
+++ server/config.js
@@ -0,0 +1,9 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    'mongodb://localhost/art_auction',
+  port: 3030,
+  secret: 'RamtF3BAC8E761687B25F61D95100537A6FB1254E3DAB50A078C68BF575E8B13054CejaRepaka2012312321'
+};
+
